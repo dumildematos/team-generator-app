e85ba5d61266f2ac4ef76b08bd6d82746184cb1b
Dumilde Matos <dumildejosematos@gmail.com>
2022-03-08 13:04:02 +0100

    creat team random generating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'team-generator-app';
+
+  newMemberName: string ="";
+  members: string[] = [];
+  errorMessage = "";
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
+
+
+  addMember () {
+
+    if(!this.newMemberName){
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    this.errorMessage = "";
+    
+  }
+
+  onInput(member: string){
+    this.newMemberName = member;
+    // console.log(member)
+  }
+  onNumberOfTeamsInput(number: string) {
+    this.numberOfTeams = Number(number)
+    console.log(number)
+  }
+  
+  generateTeams(){
+
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of teams";
+      return;
+    }
+    
+    if(this.members.length < this.numberOfTeams){
+      this.errorMessage = "Not enough members";
+      return;
+    }
+
+    const allMembers = [...this.members];
+    this.errorMessage = "";
+
+    while(allMembers.length){
+      for(let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+        
+        if(!member) break;
+
+        if(this.teams[i]){
+          this.teams[i].push(member)
+        }else {
+          this.teams[i] = [member]
+        }
+  
+      }
+    }
+    
+    this.members = [];
+    this.numberOfTeams = "";
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule
